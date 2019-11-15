3b9a1516121cc37fe4feb40fc24993f26f2fc5c1
Zander Martineau <hi@zander.wtf>
2019-11-15 19:31:54 +0000

    feat: update deps & add intro story
    Signed-off-by: Zander Martineau <hi@zander.wtf>

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addDecorator } from '@storybook/react'
 import { getStorybook, addParameters } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
 import React from 'react'
 import { ThemeProvider } from 'theme-ui'
 import { theme } from '../src/tokens/themeUiTheme'
@@ -15,13 +16,14 @@ addParameters({
     { name: 'Grey', value: '#444' },
   ],
 })
+addDecorator(withA11y)
 addDecorator(withKnobs)
 addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>)
 
 configure(
   [
-    // require('../src/info.story'),
-    require.context('../src/', true, /stor(ies|y)\.(tsx|ts|js|mdx)$/),
+    require.context('../src/', true, /stor(ies|y)\.mdx$/),
+    require.context('../src/', true, /stor(ies|y)\.(tsx|ts|js)$/),
   ],
   module
 )
--- src/info.story.ts
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import { doc } from 'storybook-readme'
-import README from '../README.md'
-
-storiesOf('Welcome', module).add('to the React Design System', doc(README))
