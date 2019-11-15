aa3795a8c5860847ba615a0a52047a45fb30b466
Zander Martineau <hi@zander.wtf>
2019-11-15 22:56:57 +0000

    fix jest tests
    Signed-off-by: Zander Martineau <hi@zander.wtf>

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,11 @@ module.exports = {
   testPathIgnorePatterns: [
     '<rootDir>/cypress',
     '<rootDir>/node_modules',
-    '<rootDir>/build'
+    '<rootDir>/build',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: [
-    'jest-styled-components',
-    'jest-dom/extend-expect',
-    '@testing-library/react/cleanup-after-each'
-  ]
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  snapshotSerializers: ['jest-emotion'],
 }
